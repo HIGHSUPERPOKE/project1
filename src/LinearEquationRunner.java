4beee5bf9950e85b979d9180b792ac33f1d59304
Alex Liu <alexl171@nycstudents.net>
2024-11-04 01:18:08 -0500

    initial

--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -2,6 +2,7 @@ public class LinearEquationRunner {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("goodbye");
+        System.out.println("hello");
     }
 
 }
